4910d2469581eec85549f38972babe47cf3e9583
Guy G <guyg@Guys-MacBook-Pro.local>
2017-08-28 19:35:50 +0300

    basic login, removed config for security measures

---
+++ android/app/src/main/java/com/loginv3/MainApplication.java
@@ -0,0 +1,44 @@
+package com.loginv3;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.airlabsinc.RNAWSCognitoPackage;
+import com.amazonaws.reactnative.core.AWSRNCorePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNAWSCognitoPackage(),
+            new AWSRNCorePackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
